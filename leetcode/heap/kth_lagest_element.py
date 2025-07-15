a5e9405d7149726dc64d77939872e681adcee334
juancoquet <juancoquet@gmail.com>
2025-07-15 22:44:10 +0100

    feat: implement KthLargest class with heap-based solution
    Signed-off-by: juancoquet <juancoquet@gmail.com>

--- leetcode/heap/kth_lagest_element.py
+++ leetcode/heap/kth_lagest_element.py
@@ -1,13 +1,28 @@
 # https://leetcode.com/problems/kth-largest-element-in-a-stream/description/
 
+import heapq
 from typing import List
 
 
-# given k and a stream of scores, continuously return the kth largest on each addition
 class KthLargest:
     def __init__(self, k: int, nums: List[int]) -> None:
-        return
+        self._k = k
+        self._heap = nums
+        heapq.heapify(self._heap)
+        while len(self._heap) > k:
+            self.pop()
 
     def add(self, val: int) -> int:
-        # note that k is 1-indexed
-        return 0
+        if len(self._heap) < self._k:
+            heapq.heappush(self._heap, val)
+        elif val > self.peek():
+            heapq.heappushpop(self._heap, val)
+        return self.peek()
+
+    def pop(self) -> int:
+        num = self._heap.pop(0)
+        heapq.heapify(self._heap)
+        return num
+
+    def peek(self) -> int:
+        return self._heap[0]
